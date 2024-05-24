4d79bf6a048cc95d9ac1bc234104bc42763eba99
harshitChauhan001 <108900696+harshitChauhan001@users.noreply.github.com>
2024-05-24 22:49:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const session = require("express-session");
 
 const mongooseConnection = require("./src/db/mongoose.js");
 
-const UserRoutes = require("../server/src/routes/users.js");
-const MovieRoutes = require("../server/src/routes/movies.js");
-const TheaterRoutes = require("../server/src/routes/theater.js");
-const ShowTimeRoutes = require("../server/src/routes/showTime.js");
-const SeatRoutes = require("../server/src/routes/seat.js");
-const TicketRoutes = require("../server/src/routes/ticket.js");
+const UserRoutes = require("./src/routes/users.js");
+const MovieRoutes = require("./src/routes/movies.js");
+const TheaterRoutes = require("./src/routes/theater.js");
+const ShowTimeRoutes = require("./src/routes/showTime.js");
+const SeatRoutes = require("./src/routes/seat.js");
+const TicketRoutes = require("./src/routes/ticket.js");
 
 const app = express();
 const Port = process.env.PORT || 3000;
