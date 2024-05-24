4b69a99c789a1f503e5839de386ba2d69f716e1d
harshitChauhan001 <108900696+harshitChauhan001@users.noreply.github.com>
2024-05-24 22:50:28 +0530

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const { MONGO_USER, MONGO_PASSWORD, MONGO_CLUSTER, MONGO_DATABASE } =
   process.env;
 
-const CONNECTION_URL = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_CLUSTER}/${MONGO_DATABASE}?retryWrites=true&w=majority`;
+const CONNECTION_URL = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_CLUSTER}/${MONGO_DATABASE}?retryWrites=true&w=majority&appName=movie-ticket-cluster`;
 // const CONNECTION_URL =
 //   "mongodb+srv://harshit:W5JSUe0r5cISUryd@movie-ticket-cluster.y9ckedj.mongodb.net/movie_ticket_db?retryWrites=true&w=majority&appName=movie-ticket-cluster";
 
